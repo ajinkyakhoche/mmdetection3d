37d61b860da500ece769709956e895b5b4979349
Ajinkya <khoche@kth.se>
2021-09-24 18:11:43 +0200

    add new classes to __init__.py

--- mmdet3d/core/visualizer/__init__.py
+++ mmdet3d/core/visualizer/__init__.py
@@ -2,4 +2,10 @@
 from .show_result import (show_multi_modality_result, show_result,
                           show_seg_result)
 
-__all__ = ['show_result', 'show_seg_result', 'show_multi_modality_result']
+"""Visualizer for 3D ML."""
+
+from .boundingbox import *
+from .colormap import *
+from .visualizer import *
+
+__all__ = ['show_result', 'show_seg_result', 'show_multi_modality_result', 'boundingbox', 'colormap', 'visualizer']
