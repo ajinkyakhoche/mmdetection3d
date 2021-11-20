609d730169462facd166f7c9802389ae656bd580
Ajinkya Khoche <khoche@kth.se>
2021-11-20 14:48:08 +0100

    update __init__ for  detector, middle_encoder, neck and decode_head

--- mmdet3d/models/decode_heads/__init__.py
+++ mmdet3d/models/decode_heads/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .paconv_head import PAConvHead
 from .pointnet2_head import PointNet2Head
+from .motion_prediction_head import MotionPrediction
 
-__all__ = ['PointNet2Head', 'PAConvHead']
+__all__ = ['PointNet2Head', 'PAConvHead', 'MotionPrediction']
--- mmdet3d/models/middle_encoders/__init__.py
+++ mmdet3d/models/middle_encoders/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .pillar_scatter import PointPillarsScatter
+from .pillar_scatter_temporal import PointPillarsScatterTemporal
 from .sparse_encoder import SparseEncoder
 from .sparse_unet import SparseUNet
 
-__all__ = ['PointPillarsScatter', 'SparseEncoder', 'SparseUNet']
+__all__ = ['PointPillarsScatter', 'PointPillarsScatterTemporal', 'SparseEncoder', 'SparseUNet']
